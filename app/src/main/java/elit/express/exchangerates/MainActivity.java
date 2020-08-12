bbf006c86c64f3a7b43257bd25a68434fd4c4391
YevheniiSych <66691210+YevheniiSych@users.noreply.github.com>
2020-08-12 16:10:08 +0300

    Checking internet on loading activity

--- app/src/main/java/elit/express/exchangerates/MainActivity.java
+++ app/src/main/java/elit/express/exchangerates/MainActivity.java
@@ -2,7 +2,15 @@ package elit.express.exchangerates;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(isNetworkAvailable()) {
+            //Toast.makeText(this, "nice", Toast.LENGTH_LONG).show();
+            Intent intent=new Intent("RatesActivity");
+            startActivity(intent);
+        }
+        else {
+            AlertDialog.Builder aBuilder = new AlertDialog.Builder(this);
+            aBuilder.setMessage(R.string.checkInternet)
+                    .setCancelable(true)
+                    .setNegativeButton(R.string.ok,
+                            new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            finish();
+                        }
+                    });
+            AlertDialog alert = aBuilder.create();
+            alert.setTitle(R.string.connectionError);
+            alert.show();
+        }
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivity = (ConnectivityManager) this
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity == null) {
+            Log.d("NetworkCheck", "isNetworkAvailable: No");
+            return false;
+        }
+
+        // get network info for all of the data interfaces (e.g. WiFi, 3G, LTE, etc.)
+        NetworkInfo[] info = connectivity.getAllNetworkInfo();
+
+        // make sure that there is at least one interface to test against
+        if (info != null) {
+            // iterate through the interfaces
+            for (int i = 0; i < info.length; i++) {
+                // check this interface for a connected state
+                if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                    Log.d("NetworkCheck", "isNetworkAvailable: Yes");
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
